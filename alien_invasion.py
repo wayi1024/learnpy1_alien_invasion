c3c0d68344a6bec73e1ba47cc0aebd39b0e179b2
wushi <123456789@qq.com>
2022-06-14 11:14:04 +0800

    use class Settings to modify alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,17 +2,18 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
+        self.settings=Settings()
 
-        self.screen=pygame.display.set_mode((1200,800))
+        self.screen=pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        #设置背景色
-        self.bg_color=(230,230,230)
     
     def run_game(self):
         """开始游戏的主循环"""
@@ -23,7 +24,7 @@ class AlienInvasion:
                     sys.exit()
             
             #每次循环都重绘屏幕
-            pygame.display.fill(self.bg_color)
+            pygame.display.fill(self.settings.bg_color)
             
             #让最近绘制的屏幕可见
             pygame.display.flip()
